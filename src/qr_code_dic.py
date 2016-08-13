159723f32973c61797444fe0d6f89508f22aff9d
Sugesh Chandran <sugeshchandran@gmail.com>
2016-08-13 20:19:03 +0100

    Adding a dictionary to store QR-code <-> video mappings
    Signed-off-by: Sugesh Chandran <sugeshchandran@gmail.com>

---
+++ src/qr_code_dic.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python
+# -*- coding: utf8 -*-
+# The statis qr code to video mapping, will extend to use DB in future. 
+#
+__author__ = "Sugesh Chandran"
+__copyright__ = "Copyright (C) The neoview team."
+__license__ = "GNU Lesser General Public License"
+__version__ = "1.0"
+
+'''
+Dictionary to store the video links for every QR code, This dictionary used to
+play specific video based on QR code than scanned by the camera.
+'''
+qr_video_dic = {
+                "1" : "https://www.youtube.com/watch?v=y6SD0QQN28Q",
+                "2" : "https://www.youtube.com/watch?v=Wf-UtiLLrL8",
+                "7825101034666317" : "https://www.youtube.com/watch?v=Wf-UtiLLrL8"
+            }
